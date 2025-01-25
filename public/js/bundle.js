ab492c2e88ed9b89912b796cb4bbacfed0a42341
Natty Lar <cloud999.nl@gmail.com>
2025-01-25 21:29:04 +0100

    Add final features to home page

---
+++ public/js/bundle.js
@@ -0,0 +1,391 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"index.js":[function(require,module,exports) {
+function _toConsumableArray(r) { return _arrayWithoutHoles(r) || _iterableToArray(r) || _unsupportedIterableToArray(r) || _nonIterableSpread(); }
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
+function _iterableToArray(r) { if ("undefined" != typeof Symbol && null != r[Symbol.iterator] || null != r["@@iterator"]) return Array.from(r); }
+function _arrayWithoutHoles(r) { if (Array.isArray(r)) return _arrayLikeToArray(r); }
+function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
+/* eslint-disable */
+
+function intersectionObserver(animatedEls) {
+  var observer = new IntersectionObserver(function (entries) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        animatedEls.forEach(function (el) {
+          return el.classList.add('slide-up');
+        });
+        observer.unobserve(entry.target); // Stop observing after in view
+      }
+    });
+  });
+  observer.observe(animatedEls[animatedEls.length - 1]);
+}
+function scrollTo(clickedEl, viewEl) {
+  clickedEl.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: viewEl.offsetTop - 89.59,
+      // Adjust based on your fixed nav height
+      behavior: "smooth"
+    });
+  });
+}
+document.addEventListener('DOMContentLoaded', function () {
+  var videoEl = document.getElementById('myVideo');
+  var meshEl = document.querySelector('.hero__mesh');
+  var awardSectionEl = document.querySelector('.award-section');
+  var scrollUpAnimatedAwardsEls = document.querySelectorAll('.scroll-up-animated--awards');
+  var scrollToServicesBtnEl = document.getElementById('scrollToServicesBtn');
+  var homeLinkEl = document.getElementById('homeLink');
+  var servicesLinkEl = document.getElementById('servicesLink');
+  var aboutLinkEl = document.getElementById('aboutLink');
+  var projectsLinkEl = document.getElementById('projectsLink');
+  var contactLinkEl = document.getElementById('contactLink');
+  var servicesSectionEl = document.getElementById('servicesSection');
+  var aboutSectionEl = document.getElementById('aboutSection');
+  var projectsSectionEl = document.getElementById('projectsSection');
+  var contactSectionEl = document.getElementById('contactSection');
+  var projectsSet1Els = document.querySelectorAll('.projects-set--1');
+  var projectsSet2Els = document.querySelectorAll('.projects-set--2');
+  var projectsSet3Els = document.querySelectorAll('.projects-set--3');
+  meshEl.style.display = 'none';
+
+  // Remove blur and show mesh when the video is ready to play
+  videoEl.addEventListener('canplaythrough', function () {
+    videoEl.style.filter = 'blur(0)'; // Remove blur
+    meshEl.style.display = 'block'; // Show mesh
+  });
+
+  // Scroll to services with arrow
+  if (scrollToServicesBtnEl) {
+    scrollTo(scrollToServicesBtnEl, servicesSectionEl);
+  }
+
+  // Scroll to home
+  if (homeLinkEl) {
+    homeLinkEl.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector('.page-header').scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  }
+
+  // Scroll to services with link
+  if (servicesLinkEl) {
+    scrollTo(servicesLinkEl, servicesSectionEl);
+  }
+
+  // Scroll to about sections
+  if (aboutLinkEl) {
+    scrollTo(aboutLinkEl, aboutSectionEl);
+  }
+
+  // Scroll to about sections
+  if (projectsLinkEl) {
+    scrollTo(projectsLinkEl, projectsSectionEl);
+  }
+
+  // Scroll to about sections
+  if (contactLinkEl) {
+    scrollTo(contactLinkEl, contactSectionEl);
+  }
+  if (awardSectionEl) {
+    var awardsArray = _toConsumableArray(scrollUpAnimatedAwardsEls);
+    awardsArray.push(awardSectionEl);
+    intersectionObserver(awardsArray);
+  }
+  intersectionObserver(projectsSet1Els);
+  intersectionObserver(projectsSet2Els);
+  intersectionObserver(projectsSet3Els);
+});
+},{}],"../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51111" + '/');
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      });
+
+      // Enable HMR for CSS by default.
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+  if (overlay) {
+    overlay.remove();
+  }
+}
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID;
+
+  // html encode message and stack trace
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+  var parents = [];
+  var k, d, dep;
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+  return parents;
+}
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+  if (checkedAssets[id]) {
+    return;
+  }
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+    return true;
+  }
+}
+},{}]},{},["../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
+//# sourceMappingURL=/bundle.js.map
---
+++ public/js/index.js
@@ -0,0 +1,96 @@
+/* eslint-disable */
+
+function intersectionObserver(animatedEls) {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        animatedEls.forEach(el => el.classList.add('slide-up'));
+        observer.unobserve(entry.target); // Stop observing after in view
+      }
+    });
+  });
+
+  observer.observe(animatedEls[animatedEls.length - 1]);
+}
+
+function scrollTo(clickedEl, viewEl) {
+  clickedEl.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({
+      top: viewEl.offsetTop - 89.59, // Adjust based on your fixed nav height
+      behavior: "smooth"
+    });
+  });
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const videoEl = document.getElementById('myVideo');
+  const meshEl = document.querySelector('.hero__mesh');
+  const awardSectionEl = document.querySelector('.award-section');
+  const scrollUpAnimatedAwardsEls = document.querySelectorAll('.scroll-up-animated--awards');
+  const scrollToServicesBtnEl = document.getElementById('scrollToServicesBtn');
+  const homeLinkEl = document.getElementById('homeLink');
+  const servicesLinkEl = document.getElementById('servicesLink');
+  const aboutLinkEl = document.getElementById('aboutLink');
+  const projectsLinkEl = document.getElementById('projectsLink');
+  const contactLinkEl = document.getElementById('contactLink');
+  const servicesSectionEl = document.getElementById('servicesSection');
+  const aboutSectionEl = document.getElementById('aboutSection');
+  const projectsSectionEl = document.getElementById('projectsSection');
+  const contactSectionEl = document.getElementById('contactSection');
+  const projectsSet1Els = document.querySelectorAll('.projects-set--1');
+  const projectsSet2Els = document.querySelectorAll('.projects-set--2');
+  const projectsSet3Els = document.querySelectorAll('.projects-set--3');
+
+  meshEl.style.display = 'none';
+
+  // Remove blur and show mesh when the video is ready to play
+  videoEl.addEventListener('canplaythrough', function () {
+    videoEl.style.filter = 'blur(0)'; // Remove blur
+    meshEl.style.display = 'block'; // Show mesh
+  });
+
+  // Scroll to services with arrow
+  if (scrollToServicesBtnEl) {
+    scrollTo(scrollToServicesBtnEl, servicesSectionEl);
+  }
+
+  // Scroll to home
+  if (homeLinkEl) {
+    homeLinkEl.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector('.page-header').scrollIntoView({ behavior: 'smooth' });
+    });
+  }
+
+  // Scroll to services with link
+  if (servicesLinkEl) {
+    scrollTo(servicesLinkEl, servicesSectionEl);
+  }
+
+  // Scroll to about sections
+  if (aboutLinkEl) {
+    scrollTo(aboutLinkEl, aboutSectionEl);
+  }
+
+  // Scroll to about sections
+  if (projectsLinkEl) {
+    scrollTo(projectsLinkEl, projectsSectionEl);
+  }
+
+  // Scroll to about sections
+  if (contactLinkEl) {
+    scrollTo(contactLinkEl, contactSectionEl);
+  }
+
+  if (awardSectionEl) {
+    const awardsArray = [...scrollUpAnimatedAwardsEls];
+    awardsArray.push(awardSectionEl);
+    intersectionObserver(awardsArray);
+  }
+
+  intersectionObserver(projectsSet1Els);
+  intersectionObserver(projectsSet2Els);
+  intersectionObserver(projectsSet3Els);
+});
